998aef119a8b1907ed19fd6f2ad217d882b79975
Nathan Wallace <dev@nathanbw.com>
2025-01-25 23:36:06 -0500

    Works on 9x9 grid now!
    * Generic code where I could manage it, special case code where I
      couldn't
    * Solved multiple bugs on the way

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::collections::HashSet;
 #[derive(Debug)]
 #[derive(Clone)]
-struct MiniDokuCell {
+struct DokuCell {
     pub index: usize,
     pub current_value: u32,
     pub is_locked: bool,
     pub potential_values: HashSet<u32>,
 }
 
-impl MiniDokuCell {
+impl DokuCell {
     fn new(index: usize) -> Self {
-        let mut new_cell = MiniDokuCell {
+        let mut new_cell = DokuCell {
             index,
             current_value: 0,
             is_locked: false,
@@ -18,45 +18,15 @@ impl MiniDokuCell {
         };
         new_cell
     }
-
-    // fn solve(&mut self, board: &mut MiniDoku) -> Result<(), String> {
-    //     let (x, y) = board.get_coords(self.index);
-    //     // Base cases:
-    //     // If I'm the last cell:
-    //     if self.index == 15 {
-    //         if self.is_locked { // If I'm locked, we're done!
-    //             return Ok(());
-    //         }
-    //         self.potential_values = board.gather_possible_values(x, y);
-    //         if self.potential_values.is_empty() {
-    //             return Err(format!("Cell at {} returning err because no possible values remain", self.index));
-    //         } else {
-    //             // There's at least one value in potential_values. There should be exactly one, or else
-    //             assert_eq!(self.potential_values.len(), 1);
-    //             self.current_value = self.potential_values.clone().into_iter().next().unwrap();
-    //             return Ok(());
-    //         }
-    //     }
-    //     // If I'm not the last cell && I'm already locked, then whether I can solve is determined
-    //     // by whether my next neighbor can solve:
-    //     if self.is_locked {
-    //         let (x, y) = board.get_coords(self.index + 1);
-    //         return board.cell_at(x, y).solve(board);
-    //     }
-    //
-    //     self.potential_values = board.gather_possible_values(x, y);
-    //
-    //     Ok(())
-    // }
 }
 
 #[derive(Clone)]
-struct MiniDoku {
-    cells: Vec<MiniDokuCell>,
-    // valid_cells: Vec<(u32, u32)>,
+struct Sudoku {
+    cells: Vec<DokuCell>,
+    dimension: usize,
 }
 
-impl MiniDoku {
+impl Sudoku {
     // This is a mini-sudoku board of size 4x4; it looks kind of like this;
     // In the diagram below, you can see which x, y pair corresponds to which
     // index in the cells vector:
@@ -75,30 +45,33 @@ impl MiniDoku {
     // Based on the above, we should be able to calculate the cell index with
     // the formula:
     // cell_index = y * 4 + x
-    fn new() -> Self {
-        let mut my_doku = MiniDoku {
+    fn new(dimension: usize) -> Self {
+        let mut my_doku = Sudoku {
             cells: Vec::new(),
+            dimension,
         };
-        for i in 0..16 {
-            my_doku.cells.push(MiniDokuCell::new(i));
+        for i in 0..(dimension * dimension) {
+            my_doku.cells.push(DokuCell::new(i));
         }
         my_doku
     }
 
-    fn cell_at(&self, x: usize, y: usize) -> &MiniDokuCell {
-        let cell_index = (y * 4) + x;
+    fn cell_at(&self, x: usize, y: usize) -> &DokuCell {
+        let cell_index = (y * self.dimension) + x;
         self.cells.get(cell_index).unwrap()
     }
 
     fn get_coords(&self, index: usize) -> (usize, usize) {
-        let x = index % 4;
-        let y = (index - x) / 4;
+        let x = index % self.dimension;
+        let y = (index - x) / self.dimension;
         (x, y)
     }
 
-    fn load_board(&mut self, board: [u32; 16]) {
-        for i in 0..16 {
-            let mut cell = self.cells.get_mut(i as usize).unwrap();
+    fn load_board(&mut self, board: Vec<u32>) {
+        assert_eq!(board.len(), (self.dimension * self.dimension),
+                   "Tried to load board of wrong dimension!");
+        for i in 0..board.len() {
+            let mut cell = self.cells.get_mut(i).unwrap();
             cell.current_value = board[i];
             cell.is_locked = true;
         }
@@ -106,58 +79,165 @@ impl MiniDoku {
 
     fn gather_possible_values(&self, x: usize, y: usize) -> HashSet<u32> {
         let mut potential_values: HashSet<u32> = HashSet::new();
-        potential_values.insert(1);
-        potential_values.insert(2);
-        potential_values.insert(3);
-        potential_values.insert(4);
-        for i_x in 0..4 {
-            if i_x == x {
+        for value in 1..(self.dimension + 1) {
+            potential_values.insert(value as u32);
+        }
+        for i in 0..self.dimension {
+            if i == x {
                 continue;
             }
-            let cell = self.cell_at(i_x, y);
+            let cell = self.cell_at(i, y);
             potential_values.remove(&cell.current_value);
         }
-        for i_y in 0..4 {
-            if i_y == y {
+        for i in 0..self.dimension {
+            if i == y {
                 continue;
             }
-            let cell = self.cell_at(x, i_y);
+            let cell = self.cell_at(x, i);
             potential_values.remove(&cell.current_value);
         }
-        // How to calculate quadrant?
-        if x < 2 {
-            if y < 2 {
-                // Top left quadrant
-                for i_x in 0..2 {
-                    for i_y in 0..2 {
-                        let cell = self.cell_at(i_x, i_y);
-                        potential_values.remove(&cell.current_value);
+        // TODO: Figure out if you can factor this better and maybe even avoid magic numbers?
+        if self.dimension == 4 {
+            // How to calculate quadrant?
+            if x < 2 {
+                if y < 2 {
+                    // Top left quadrant
+                    for i_x in 0..2 {
+                        for i_y in 0..2 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+                } else { // y >= 2
+                    // Bottom left quadrant
+                    for i_x in 0..2 {
+                        for i_y in 2..4 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
                     }
                 }
-            } else { // y >= 2
-                // Bottom left quadrant
-                for i_x in 0..2 {
-                    for i_y in 2..4 {
-                        let cell = self.cell_at(i_x, i_y);
-                        potential_values.remove(&cell.current_value);
+            } else { // x >= 2
+                if y < 2 {
+                    // Top right quadrant
+                    for i_x in 2..4 {
+                        for i_y in 0..2 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+                } else { // y >= 2
+                    // Bottom left quadrant
+                    for i_x in 2..4 {
+                        for i_y in 2..4 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
                     }
                 }
             }
-        } else { // x >= 2
-            if y < 2 {
-                // Top right quadrant
-                for i_x in 2..4 {
-                    for i_y in 0..2 {
-                        let cell = self.cell_at(i_x, i_y);
-                        potential_values.remove(&cell.current_value);
+        }
+        //   X:   0   1   2    3   4   5    6   7   8
+        //      +---+---+---++---+---+---++---+---+---+
+        // Y: 0 | 0 | 1 | 2 || 3 | 4 | 5 || 6 | 7 | 8 |
+        //      |---+---+---+|---+---+---+|---+---+---+
+        //    1 | 9 | 10| 11|| 12| 13| 14|| 15| 16| 17|
+        //      |---+---+---+|---+---+---+|---+---+---+
+        //    2 | 18| 10| 20|| 21| 22| 23|| 8 | 9 | 10|
+        //      |===+===+===+|===+===+===+|===+===+===+
+        //    3 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14|
+        //      |---+---+---+|---+---+---+|---+---+---+
+        //    4 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14|
+        //      |---+---+---+|---+---+---+|---+---+---+
+        //    5 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14|
+        //      |===+===+===+|===+===+===+|===+===+===+
+        //    6 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14|
+        //      +---+---+---++---+---+---++---+---+---+
+        //    7 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14|
+        //      +---+---+---++---+---+---++---+---+---+
+        //    8 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14|
+        //      +---+---+---++---+---+---++---+---+---+
+
+        if self.dimension == 9 {
+            if x < 3 {
+                if y < 3 {
+                    // Top left quadrant
+                    for i_x in 0..3 {
+                        for i_y in 0..3 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+                } else if y >= 3 && y < 6 {
+                    // Middle left quadrant
+                    for i_x in 0..3 {
+                        for i_y in 3..6 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+
+                } else { // y >= 6
+                    // Bottom left quadrant
+                    for i_x in 0..3 {
+                        for i_y in 6..9 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
                     }
                 }
-            } else { // y >= 2
-                // Bottom left quadrant
-                for i_x in 2..4 {
-                    for i_y in 2..4 {
-                        let cell = self.cell_at(i_x, i_y);
-                        potential_values.remove(&cell.current_value);
+            } else if x >= 3 && x < 6 {
+                if y < 3 {
+                    // Top middle quadrant
+                    for i_x in 3..6 {
+                        for i_y in 0..3 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+                } else if y >= 3 && y < 6 {
+                    // Middle middle quadrant
+                    for i_x in 3..6 {
+                        for i_y in 3..6 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+
+                } else { // y >= 6
+                    // Bottom middle quadrant
+                    for i_x in 3..6 {
+                        for i_y in 6..9 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+                }
+            } else { // x >= 6
+                if y < 3 {
+                    // Top right quadrant
+                    for i_x in 6..9 {
+                        for i_y in 0..3 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+                } else if y >= 3 && y < 6 {
+                    // Middle right quadrant
+                    for i_x in 6..9 {
+                        for i_y in 3..6 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
+                    }
+
+                } else { // y >= 6
+                    // Bottom right quadrant
+                    for i_x in 6..9 {
+                        for i_y in 6..9 {
+                            let cell = self.cell_at(i_x, i_y);
+                            potential_values.remove(&cell.current_value);
+                        }
                     }
                 }
             }
@@ -171,7 +251,7 @@ impl MiniDoku {
         let mut potential_values = HashSet::new();
         // Base cases:
         // If we're on the last cell:
-        if index == 15 {
+        if index == ((self.dimension * self.dimension) - 1) {
             if cell.is_locked { // If the last cell is locked, we're done!
                 return Ok(());
             }
@@ -203,7 +283,7 @@ impl MiniDoku {
                         return Ok(());
                     }
                     Err(_) => {
-
+                        self.cells.get_mut(index).unwrap().current_value = 0;
                     }
                 }
             }
@@ -214,33 +294,87 @@ impl MiniDoku {
     }
 }
 
-impl std::fmt::Display for MiniDoku {
+impl std::fmt::Display for Sudoku {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         // TODO: All the write! macro invocations here should return their result
         //       if it is an error. Does `?` do that? or something else?
-        for y in 0..4 {
-            if y == 2 {
-                write!(f, "|===+===++===+===|\n")?;
-            } else {
-                write!(f, "+---+---++---+---+\n")?;
-            }
-            for x in 0..4 {
-                if x == 1 || x == 3 {
-                    write!(f, "| {} |", self.cell_at(x, y).current_value)?;
+        // TODO: Can I make this generic?
+        if self.dimension == 4 {
+            for y in 0..4 {
+                if y == 2 {
+                    write!(f, "|===+===++===+===|\n")?;
                 } else {
-                    write!(f, "| {} ", self.cell_at(x, y).current_value)?;
+                    write!(f, "+---+---++---+---+\n")?;
                 }
-                if x == 3 {
-                    write!(f, "\n")?;
+                for x in 0..4 {
+                    if x == 1 || x == 3 {
+                        write!(f, "| {} |", self.cell_at(x, y).current_value)?;
+                    } else {
+                        write!(f, "| {} ", self.cell_at(x, y).current_value)?;
+                    }
+                    if x == 3 {
+                        write!(f, "\n")?;
+                    }
                 }
             }
+            return write!(f, "+---+---++---+---+")
         }
-        write!(f, "+---+---++---+---+")
+        if self.dimension == 9 {
+            //   X:   0   1   2    3   4   5    6   7   8
+            //      +===+===+===++===+===+===++===+===+===++
+            // Y: 0 | 0 | 1 | 2 || 3 | 4 | 5 || 6 | 7 | 8 ||
+            //      |---+---+---+|---+---+---++---+---+---+|
+            //    1 | 9 | 10| 11|| 12| 13| 14|| 15| 16| 17||
+            //      |---+---+---+|---+---+---++---+---+---+|
+            //    2 | 18| 10| 20|| 21| 22| 23|| 8 | 9 | 10||
+            //      |===+===+===+|===+===+===++===+===+===+|
+            //    3 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14||
+            //      |---+---+---+|---+---+---++---+---+---+|
+            //    4 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14||
+            //      |---+---+---+|---+---+---++---+---+---+|
+            //    5 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14||
+            //      |===+===+===+|===+===+===++===+===+===+|
+            //    6 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14||
+            //      +---+---+---++---+---+---++---+---+---++
+            //    7 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14||
+            //      +---+---+---++---+---+---++---+---+---++
+            //    8 | 12| 13| 14|| 12| 13| 14|| 12| 13| 14||
+            //      +===+===+===+|===+===+===++===+===+===++
+            for y in 0..9 {
+                if y % 3 == 0 {
+                    write!(f, "||===+===+===++===+===+===++===+===+===||\n")?;
+                } else {
+                    write!(f, "++---+---+---++---+---+---++---+---+---++\n")?;
+                    // write!(f, "\n")?;
+                }
+                for x in 0..9 {
+                    if x == 0 {
+                        write!(f, "|");
+                    }
+                    if x % 3 == 2 {
+                        write!(f, "| {} |", self.cell_at(x, y).current_value)?;
+                    } else {
+                        write!(f, "| {} ", self.cell_at(x, y).current_value)?;
+                    }
+                    if x == 8 {
+                        write!(f, "|\n")?;
+                    }
+                }
+            }
+            return write!(f, "||===+===+===++===+===+===++===+===+===||\n");
+
+        }
+        write!(f, "Not implemented for dimension {}", self.dimension)
     }
 }
 
 fn main() {
-    let mut my_doku = MiniDoku::new();
+    // let puzzle: Vec<u32> = vec![
+    //     1, 2, 3, 4
+    //     3, 4, 1, 2
+    //
+    // ];
+    let mut my_doku = Sudoku::new(9);
     // println!("{my_doku}");
     // println!("{}", my_doku.cell_at(3, 2).current_value);
     match my_doku.solve(0) {
